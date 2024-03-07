6c825f44cb76e404f4c113efba7c6a0698325397
MacBooK Pro <holamarialazaro@gmail.com>
2024-03-07 17:06:57 +0100

    instalado redux y creado store y slice users

---
+++ src/pages/userSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {},
+    reducers: {
+        login: (state, action) => {
+            return {
+                ...state,
+                ...action.payload
+            }
+        },
+        logout: (state,action) => {
+         return{}
+        }
+    
+    }
+
+})
+
+export const {login, logout } = userSlice.actions;
+export const userData = (state) => state.user;
+export default userSlice.reducer;
